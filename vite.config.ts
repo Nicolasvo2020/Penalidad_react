ef1be15a65c187444f2379f7d6aefffc79163a2d
Nicolasvo2020 <nvelascoocampo@gmail.com>
2025-01-29 09:40:57 -0500

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://Nicolasvo2020.github.io/Penalidad_react"
 })
